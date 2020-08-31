2619130867303bb8d0833fe63b872a8912480f64
nivedkrishnans <37117180+nivedkrishnans@users.noreply.github.com>
2020-08-31 19:59:49 +0530

    created a simple colored image

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import png
 import os
+from datetime import datetime
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-output_dir = os.path.join(current_dir,'output')
+output_dir = os.path.join(current_dir,'output', str(datetime.now()))
 
 def create_output_dir():
     condition = not os.path.exists(output_dir)
@@ -10,9 +11,26 @@ def create_output_dir():
         os.makedirs(output_dir)
     return output_dir
 
-create_output_dir()
-f = open(os.path.join(output_dir, 'ramp.png'), 'wb')
-w = png.Writer(256, 1, greyscale=True)
-w.write(f, [range(256)])
-f.close()
 
+f = open(os.path.join(create_output_dir() ,'ramp.png'), 'wb')
+
+s = []
+a = []
+print(type(a))
+bitdepth = 7
+for i in range(2**bitdepth):
+    for j in range(2**bitdepth):
+        for k in range(2**bitdepth):
+            for l in range(2**bitdepth):
+                s.append(int(i/2))
+                s.append(j)
+                s.append(k)
+                s.append(l)
+        a.append(tuple(s))
+        s=[]
+        print('row ', (j+1), ' completed')
+
+w = png.Writer(width = 4**bitdepth, height = 4**bitdepth, greyscale=False, alpha = True, bitdepth=bitdepth, )
+# print(a)
+w.write(f, a)
+f.close()
